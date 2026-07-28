1a59d0095afab82834ab4cc4aaeb8123a9c76f48
mechacoredev <eneserbulut67890@gmail.com>
2026-07-28 13:50:28 +0300

    Update stm32f4xx_it.h

--- Inc/stm32f4xx_it.h
+++ Inc/stm32f4xx_it.h
@@ -52,6 +52,7 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
+void EXTI1_IRQHandler(void);
 void DMA1_Stream0_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void I2C1_EV_IRQHandler(void);
