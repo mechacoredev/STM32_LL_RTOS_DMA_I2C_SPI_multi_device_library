1d1ea12424432fb160cc7eb49f35b422456147f2
mechacoredev <eneserbulut67890@gmail.com>
2026-08-01 13:55:05 +0300

    Update nrf24l01.h

--- Inc/nrf24l01.h
+++ Inc/nrf24l01.h
@@ -6,12 +6,35 @@
 #include "stdint.h"
 #include "stddef.h"
 
+#define NRF24L01_PAYLOAD_SIZE 32
+#define NRF24L01_FIFO_DEPTH   3
+
+#define NRF24L01_IRQ_RX_DR  0x40
+#define NRF24L01_IRQ_TX_DS  0x20
+#define NRF24L01_IRQ_MAX_RT 0x10
+
 typedef enum{
 	_nrf24l01_ok = 0,
 	_nrf24l01_fail,
 	_nrf24l01_timeout,
 	_nrf24l01_device_not_found
 }nrf24l01_return_status;
+
+typedef struct{
+	uint8_t raw;
+	bool rx_dr;
+	bool tx_ds;
+	bool max_rt;
+}nrf24l01_irq_status_t;
+
+typedef struct{
+	uint8_t raw;
+	bool tx_reuse;
+	bool tx_full;
+	bool tx_empty;
+	bool rx_full;
+	bool rx_empty;
+}nrf24l01_fifo_status_t;
 
 // Kullanıcı Konfigürasyonu (bme280_user_configs gibi)
 typedef struct{
@@ -41,6 +64,17 @@ nrf24l01_handle_t nrf24l01_init(nrf24l01_user_configs* config);
 // DMA Tabanlı Asenkron Veri Transferleri
 nrf24l01_return_status nrf24l01_send_dma(nrf24l01_handle_t dev, uint8_t *data, uint8_t length);
 nrf24l01_return_status nrf24l01_receive_dma(nrf24l01_handle_t dev, uint8_t *data, uint8_t length);
+
+// SPI DMA worker task ile kullanilan job tabanli API
+nrf24l01_return_status nrf24l01_build_tx_job(nrf24l01_handle_t dev, const uint8_t *data, uint8_t length,
+		osThreadId_t notify_task, uint32_t notify_flag, uint32_t error_flag, spi_job_t* out_job);
+nrf24l01_return_status nrf24l01_build_rx_fifo_job(nrf24l01_handle_t dev,
+		osThreadId_t notify_task, uint32_t notify_flag, uint32_t error_flag, spi_job_t* out_job);
+nrf24l01_return_status nrf24l01_finish_rx_fifo_job(nrf24l01_handle_t dev, uint8_t *data, uint8_t length);
+nrf24l01_return_status nrf24l01_trigger_transmission(nrf24l01_handle_t dev);
+nrf24l01_return_status nrf24l01_get_irq_status(nrf24l01_handle_t dev, nrf24l01_irq_status_t* out_status);
+nrf24l01_return_status nrf24l01_get_fifo_status(nrf24l01_handle_t dev, nrf24l01_fifo_status_t* out_status);
+void nrf24l01_clear_irq_sources(nrf24l01_handle_t dev, uint8_t irq_mask);
 
 // Durum Kontrolleri
 void nrf24l01_clear_interrupts(nrf24l01_handle_t dev);
